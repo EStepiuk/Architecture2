eef4d5b5ce7b15a64a808cdf95ac49badd8b3907
Meggapixxel <android.vadim@gmail.com>
2016-04-22 09:38:48 +0300

    view

---
+++ views/view.py
@@ -0,0 +1,41 @@
+__author__ = 'Meggapixxel'
+
+class ConsoleView(object):
+    """
+    Class shows info on console
+    """
+
+    def render_matches(self, matches):
+        """
+        Show list of match
+        :param matches:
+        :return:
+        """
+        for match in matches:
+            print(match)
+
+    def render_menu(self):
+        """
+        Show menu cases
+        """
+        print("""
+        1. England
+        2. Spain
+        3. Ukraine
+        4. Select team
+        5. Add match
+        6. Load matches from file
+        7. Show all matches
+        8. Exit
+        """)
+
+    def console_input(self):
+        """
+        Input from console
+        :return:
+        """
+        return input()
+
+    def msg_output(self, mes):
+        print(mes)
+
